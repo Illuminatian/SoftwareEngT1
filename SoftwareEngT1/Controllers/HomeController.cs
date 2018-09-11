4436722640068187ce21e459f4f2adcf9b303552
Jason Jacobs <jacobs.jb@gmail.com>
2018-09-11 18:10:27 -0500

    final updates to functionality for scheduler

--- SoftwareEngT1/Controllers/HomeController.cs
+++ SoftwareEngT1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SoftwareEngT1.Models;
 
 namespace SoftwareEngT1.Controllers
@@ -30,21 +31,109 @@ namespace SoftwareEngT1.Controllers
             {
                 foreach(var schedule in db.Schedules)
                 {
-                    var toAdd = new Schedule();
-                    toAdd.AppointmentTime = schedule.AppointmentTime;
-                    toAdd.AppointmentDate = schedule.AppointmentDate;
-
-                    toAdd.AppointmentType = schedule.AppointmentType;
-                    toAdd.FirstName = schedule.FirstName;
-                    toAdd.LastName = schedule.LastName;
-                    toAdd.PhoneNumber = schedule.PhoneNumber;
-                    toAdd.EmailAddress = schedule.EmailAddress;
-
-                    model.Add(toAdd);
+                    model.Add(schedule);
                 }
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Schedule schedule)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(schedule);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Scheduler));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(schedule);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var customer = await _context.Schedules.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            var customer = await _context.Schedules.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var customer = await _context.Schedules.SingleOrDefaultAsync(m => m.Id == id);
+
+            try
+            {
+                _context.Remove(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Scheduler));
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Schedule schedule)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Update(schedule);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Scheduler));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+            return View(schedule);
         }
 
         public IActionResult Contact()
--- SoftwareEngT1/Models/ScheduleModel.cs
+++ SoftwareEngT1/Models/ScheduleModel.cs
@@ -31,12 +31,22 @@ namespace SoftwareEngT1.Models
         [Key]
         public int Id { get; set; }
         [DataType("Date")]
+        [Display(Name="Appointment Date:")]
         public DateTime AppointmentDate { get; set; }
+        [DataType("Time")]
+        [Display(Name = "Appointment Time:")]
         public DateTime AppointmentTime { get; set; }
+        [Display(Name = "First Name:")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name:")]
         public string LastName { get; set; }
+        [Display(Name = "Appointment Type:")]
         public string AppointmentType { get; set; }
+        [DataType("PhoneNumber")]
+        [Display(Name = "Phone Number:")]
         public string PhoneNumber { get; set; }
+        [DataType("EmailAddress")]
+        [Display(Name = "Email Address:")]
         public string EmailAddress { get; set; }
 
         public Schedule()
